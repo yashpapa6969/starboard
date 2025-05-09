651ad065bb3821f88853fb199e8211bd7f4edd16
yashpapa6969 <yashgupta.yg856@gmail.com>
2025-05-09 11:47:53 +0530

    Update API base URL in pdfService to use Vite environment variable

--- starboard/src/services/pdfService.ts
+++ starboard/src/services/pdfService.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-const API_BASE_URL = `${process.env.API_BASE_URL}`;
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`;
 
 export interface UploadUrlResponse {
   uploadUrl: string;
